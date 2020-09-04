974fccfa379791f497fb7381e6a968c272ceedca
chipfody <60615532+chipfody@users.noreply.github.com>
2020-09-04 10:50:39 -0400

    added Stirng reversal

---
+++ KnowledgeBuilder/src/uDemyProjects/ReverseString.java
@@ -0,0 +1,32 @@
+package uDemyProjects;
+
+import java.util.Scanner;
+
+public class ReverseString {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter a word to reverse:");
+        String word = scanner.next();
+        System.out.println("The word " + word + " reversed in Method A is: " + reverseMethodA(word));
+        System.out.println("The word " + word + " reversed in Method B is: " + reverseMethodB(word));
+
+        }
+
+    static String reverseMethodA (String toBeRev) {
+        StringBuilder revWord = new StringBuilder(toBeRev);
+        return revWord.reverse().toString();
+    }
+
+    static String reverseMethodB (String toBeRev) {
+        int length = toBeRev.length();
+        String revWord = "";
+        for (int i = length - 1; i >= 0; i--) {
+            revWord  += toBeRev.charAt(i);
+        }
+        return revWord;
+    }
+
+}
