1e563bfd0c1050218d3caf44f54cfdd78be1a5f4
chipfody <60615532+chipfody@users.noreply.github.com>
2020-09-22 17:55:35 -0400

    updated method

--- KnowledgeBuilder/src/main/java/HackerRank/BegDecimalComparison.java
+++ KnowledgeBuilder/src/main/java/HackerRank/BegDecimalComparison.java
@@ -1,11 +1,18 @@
 package HackerRank;
 
+/*
+Java's BigDecimal class can handle arbitrary-precision signed decimal numbers. Let's test your knowledge of them!
+        Given an array,s, of n real number strings, sort them in descending order — but wait, there's more! Each number must be printed in the exact same format as it was read from stdin, meaning that .1 is printed as .1, and 0.1 is printed as 0.1. If two numbers represent numerically equivalent values (e.g. .1 = 0.1 ), then they must be listed in the same order as they were received as input).
+        Complete the code in the unlocked section of the editor below. You must rearrange array s's elements according to the instructions above.
+*/
+
+
 import java.math.BigDecimal;
 
-public class BegDecimalComparison {
+public class BegDecimalComparison (String[] s) {
 
-     for (int j = 0; j < n - 2; j++) {
-        for (int k = 1; k < n - j; k++) {
+     for (int j = 0; j < s.length - 2; j++) {
+        for (int k = 1; k < s.length - j; k++) {
             BigDecimal first = new BigDecimal(s[k - 1]);
             BigDecimal second = new BigDecimal(s[k]);
             if(second.compareTo(first) == 1){
