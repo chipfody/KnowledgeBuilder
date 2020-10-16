f0770f43e661479e6c58b819fbe903ff5d95e232
chipfody <60615532+chipfody@users.noreply.github.com>
2020-10-16 17:14:34 -0400

    added String manipulation

---
+++ KnowledgeBuilder/src/main/java/UdemyPuzzles/StringManipulation.java
@@ -0,0 +1,26 @@
+package UdemyPuzzles;
+
+public class StringManipulation {
+    /**
+     Given a string of odd length, return the middle 3 characters from the string,
+     so the string <b>"Monitor"</b> yields <b>"nit"</b>.
+     If the string length is less than 3, return the string as is. <br> <br>
+
+     <b>EXPECTATIONS:</b><br>
+     middleThree("bunny") <b>---></b> "unn" <br>
+     middleThree("peter") <b>---></b> "ete" <br>
+     middleThree("Jamaica") <b>---></b>"mai" <br>
+     */
+
+    public static String middleThree(String str) {
+        String result = str;
+        if (str.length() < 3) {
+            result = str;
+        } else {
+            int mid = str.length() / 2;
+            result = str.substring(mid -1, mid + 2);
+        }
+        return result;
+
+    }
+}
