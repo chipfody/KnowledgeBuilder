0ad2fd2dfbe5c3557d49d945eabfdb986c6e6bc2
chipfody <60615532+chipfody@users.noreply.github.com>
2020-10-20 15:16:39 -0400

    added cat dog count comparison method

--- KnowledgeBuilder/src/main/java/UdemyPuzzles/StringManipulation.java
+++ KnowledgeBuilder/src/main/java/UdemyPuzzles/StringManipulation.java
@@ -23,4 +23,29 @@ public class StringManipulation {
         return result;
 
     }
+
+
+/*
+    Return true if the string "cat" and "dog" appear the same number of times in the given string.
+
+    catDog("catdog") → true
+    catDog("catcat") → false
+    catDog("1cat1cadodog") → true
+*/
+
+    public static boolean catDog(String str) {
+        int catCount = 0;
+        int dogCount = 0;
+
+        for (int i = 0; i < str.length() - 2; i++) {
+            if (str.substring(i, i + 3).equals("cat")) {
+                catCount++;
+            }
+            if (str.substring(i, i + 3).equals("dog")) {
+                dogCount++;
+            }
+        }
+        return catCount == dogCount;
+    }
+
 }
--- KnowledgeBuilder/src/test/java/UdemyPuzzles/StringManipulationTest.java
+++ KnowledgeBuilder/src/test/java/UdemyPuzzles/StringManipulationTest.java
@@ -1,5 +1,8 @@
 package UdemyPuzzles;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class StringManipulationTest {
@@ -32,6 +35,37 @@ public class StringManipulationTest {
                 + "      Expected: "+ expected.toString();
         ret += "\n" + "**********************";
         return ret;
+    }
+
+    @Test
+    public void testCatDog() {
+        boolean expected1 = StringManipulation.catDog("catdog");
+        boolean expected2 = StringManipulation.catDog("catcat");
+        boolean expected3 = StringManipulation.catDog("1cat1cadodog");
+        boolean expected4 = StringManipulation.catDog("catxxdogxxxdog");
+        boolean expected5 = StringManipulation.catDog("catxdogxdogxcat");
+        boolean expected6 = StringManipulation.catDog("catxdogxdogxca");
+        boolean expected7 = StringManipulation.catDog("dogdogcat");
+        boolean expected8 = StringManipulation.catDog("dogogcat");
+        boolean expected9 = StringManipulation.catDog("dog");
+        boolean expected10 = StringManipulation.catDog("cat");
+        boolean expected11 = StringManipulation.catDog("ca");
+        boolean expected12 = StringManipulation.catDog("c");
+        boolean expected13 = StringManipulation.catDog("");
+
+        Assert.assertTrue(expected1);
+        Assert.assertFalse(expected2);
+        Assert.assertTrue(expected3);
+        Assert.assertFalse(expected4);
+        Assert.assertTrue(expected5);
+        Assert.assertFalse(expected6);
+        Assert.assertFalse(expected7);
+        Assert.assertTrue(expected8);
+        Assert.assertFalse(expected9);
+        Assert.assertFalse(expected10);
+        Assert.assertTrue(expected11);
+        Assert.assertTrue(expected12);
+        Assert.assertTrue(expected13);
     }
 
 }
