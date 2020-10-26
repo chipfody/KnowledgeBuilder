f79535a93aeca1c7e50d6321a33d26e73703da58
chipfody <60615532+chipfody@users.noreply.github.com>
2020-10-26 16:33:11 -0400

    added alternate solution to frontTimes

--- KnowledgeBuilder/src/main/java/UdemyPuzzles/StringManipulation.java
+++ KnowledgeBuilder/src/main/java/UdemyPuzzles/StringManipulation.java
@@ -83,6 +83,8 @@ public class StringManipulation {
         return nums;
     }
 
+//    Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+
     public static String stringTimes(String str, int n) {
         String newString = "";
         for (int i = 1; i <= n; i++) {
@@ -90,6 +92,10 @@ public class StringManipulation {
         }
         return newString;
     }
+
+
+//    Given a string and a non-negative int n, we'll say that the front of the string is the first 3
+//    chars, or whatever is there if the string is less than length 3. Return n copies of the front;
 
     public static String frontTimes(String str, int n) {
 
@@ -106,6 +112,15 @@ public class StringManipulation {
             }
         }
         return newString;
+
+//        Alternative solution:
+
+//        String front = str.substring(0, run);
+//
+//        String result = "";
+//        for (int i=0; i<n; i++) {
+//            result += front;
+//        }
     }
 
 }
