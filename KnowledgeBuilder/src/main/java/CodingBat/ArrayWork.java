7de56b3cea02fce8598e893b34ff759afbabbd5c
chipfody <60615532+chipfody@users.noreply.github.com>
2020-11-03 22:51:48 -0500

    added scores clump tests and method

--- KnowledgeBuilder/src/main/java/CodingBat/ArrayWork.java
+++ KnowledgeBuilder/src/main/java/CodingBat/ArrayWork.java
@@ -61,4 +61,21 @@ public class ArrayWork {
         return max - min;
     }
 
+
+/*
+    Given an array of scores sorted in increasing order, return true if the array contains 3 adjacent scores that differ
+    from each other by at most 2, such as with {3, 4, 5} or {3, 5, 5}.
+*/
+public boolean scoresClump(int[] scores) {
+    boolean result = false;
+    for (int i = 0; i < scores.length - 2; i++) {
+        if (scores[i + 1] - scores[i] <= 2 && scores [i + 2] - scores[i + 1] <= 2 && scores[i + 2] - scores[i] <= 2) {
+            result = true;
+            break;
+        }
+    }
+    return result;
+
+}
+
 }
