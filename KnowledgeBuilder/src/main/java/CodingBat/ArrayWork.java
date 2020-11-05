b04c855be8c2188bddd87611f5f1ddd2921649c9
chipfody <60615532+chipfody@users.noreply.github.com>
2020-11-04 22:14:18 -0500

    added sum13 tests and method

--- KnowledgeBuilder/src/main/java/CodingBat/ArrayWork.java
+++ KnowledgeBuilder/src/main/java/CodingBat/ArrayWork.java
@@ -78,4 +78,25 @@ public boolean scoresClump(int[] scores) {
 
 }
 
+
+   /*
+     Return the sum of the numbers in the array, returning 0 for an empty array. Except the number 13 is very unlucky, so it does not count
+     and numbers that come immediately after a 13 also do not count.
+    */
+   public int sum13(int[] nums) {
+       int sum = 0;
+       if (nums.length == 0) {
+           return sum;
+       } else {
+           for (int i = 0; i < nums.length; i++) {
+               if (nums[i] != 13) {
+                   sum += nums[i];
+               } else { i += 1;
+               }
+           }
+       }
+       return sum;
+   }
+
+
 }
