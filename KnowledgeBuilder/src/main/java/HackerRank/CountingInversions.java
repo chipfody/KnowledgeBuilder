7f865b247be7d940506f591d362bdeaa1c624b52
chipfody <60615532+chipfody@users.noreply.github.com>
2020-11-18 17:24:58 -0500

    added counting inversions method - need to resolve issues of mega-data and time

---
+++ KnowledgeBuilder/src/main/java/HackerRank/CountingInversions.java
@@ -0,0 +1,23 @@
+package HackerRank;
+
+public class CountingInversions {
+    static long countInversions(int[] arr) {
+        int count = 0;
+        int temp = 0;
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            boolean sorted = true;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr [j + 1]) {
+                    temp = arr[j];
+                    arr [j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    count++;
+                    sorted = false;
+                }
+            }
+            if (sorted) break;
+        }
+        return count;
+    }
+}
